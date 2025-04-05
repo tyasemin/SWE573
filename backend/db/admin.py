18caad64f58028aa934c1bc02a7745eb43b3e1ae
Yasemin Tangül <ytangul1@ford.com.tr>
2025-04-06 02:58:39 +0300

    enable admin on gui

--- backend/db/admin.py
+++ backend/db/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import User, Board, Node, Connection, Tag, BoardTag, Discussion, Media, Attachment, Vote, EditHistory
+
+admin.site.register(User)
+admin.site.register(Board)
+admin.site.register(Node)
+admin.site.register(Connection)
+admin.site.register(Tag)
+admin.site.register(BoardTag)
+admin.site.register(Discussion)
+admin.site.register(Media)
+admin.site.register(Attachment)
+admin.site.register(Vote)
+admin.site.register(EditHistory)
+
