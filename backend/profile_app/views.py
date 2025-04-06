4c78162e5498b7ef21d2240f61ecbe31e50224ce
Yasemin Tangül <ytangul1@ford.com.tr>
2025-04-07 00:44:57 +0300

    fix profile url

--- backend/profile_app/views.py
+++ backend/profile_app/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def profile_view(request):
-    return render(request, 'profile_app/profile.html')
+def profile(request):
+    return render(request, "profile_app/profile.html")
