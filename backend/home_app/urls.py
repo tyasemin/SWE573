15037707f254d0f4b7348aebf8c2373e32454d09
Yasemin Tangül <ytangul1@ford.com.tr>
2025-04-17 18:40:09 +0300

    change the url in home_app to redirect ip

--- backend/home_app/urls.py
+++ backend/home_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.profile, name="home"),
+    path("", views.profile, name="home_page"),
     path('logout/', views.logout_view, name='logout'),
 ]
