f81f791456a52b5ca6423ba509500ba5a1d8e2bc
Yasemin Tangül <ytangul1@ford.com.tr>
2025-05-10 21:35:00 +0300

    redirect to home page

--- backend/profile_app/urls.py
+++ backend/profile_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.profile, name="profile"),
     path('logout/', views.logout_view, name='logout'),
+    path('home/', views.home_page, name='home_page'),
 ]
--- backend/profile_app/views.py
+++ backend/profile_app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_required
 
 def profile(request):
     return render(request, "profile_app/profile.html")
+
+@login_required
+def home_page(request):
+    return render(request, "home_app/home.html")
 
 def logout_view(request):
     django_logout(request)
