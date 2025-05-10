00342e1d1477106293572f1ccfe88de711847758
Yasemin Tangül <ytangul1@ford.com.tr>
2025-05-11 02:46:45 +0300

    add new functionality to board feed

--- backend/home_app/views.py
+++ backend/home_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_required
+from .models import Board
+import random
+from django.db.models.functions import Random
 
 def profile(request):
     return render(request, "home_app/home.html")
@@ -10,11 +14,12 @@ def logout_view(request):
     request.session.flush()  
     return redirect("landing_page") 
 
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from .models import Board  # adjust based on your actual model import
-
-@login_required
 def home_page(request):
-    user_boards = Board.objects.filter(user=request.user)
-    return render(request, "home_app/home.html", {"boards": user_boards})
+    boards = Board.objects.order_by(Random())[:10]
+    random.shuffle(boards)  # shuffle in Python memory
+    boards = boards[:10]    # take 10
+
+    return render(request, "home_app/home.html", {
+        "boards": boards
+    })
+
