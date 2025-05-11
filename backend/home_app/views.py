6454dc2cc640db0984f5b770accf77ba6d6132de
Yasemin Tangül <ytangul1@ford.com.tr>
2025-05-12 00:57:07 +0300

    fix the views

--- backend/home_app/views.py
+++ backend/home_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth import logout as django_logout
 from django.contrib.auth.decorators import login_required
-from .models import Board
+from db.models import Board
 import random
 from django.db.models.functions import Random
 
