35ce915ba678d40318fd79a743a62ec0ed165457
Yasemin Tangül <ytangul1@ford.com.tr>
2025-05-12 00:51:27 +0300

    fix the bug of board

--- backend/profile_app/views.py
+++ backend/profile_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth import logout as django_logout
 from django.contrib.auth.decorators import login_required
-from board.models import Board
+from db.models import Board
 
 @login_required
 def profile(request):
