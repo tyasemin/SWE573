1f98a6e7a5b0413a21edef6cdb64763b4876fbe3
Yasemin Tangül <ytangul1@ford.com.tr>
2025-05-12 01:47:28 +0300

    fix the recent boards

--- backend/profile_app/views.py
+++ backend/profile_app/views.py
@@ -6,13 +6,12 @@ from db.models import Board
 
 @login_required
 def profile(request):
-    user = request.user
-    #latest_boards = Board.objects.filter(owner=user).order_by('-created_at')[:3]
-    latest_boards = Board.objects.all().order_by('-created_at')
-    return render(request, "profile_app/profile.html", {
-        "user": user,
+    latest_boards = Board.objects.filter(owner=request.user).order_by('-created_at')[:3]
+    return render(request, 'profile_app/profile.html', {
+        "user": request.user,
         "latest_boards": latest_boards
     })
+
 
 @login_required
 def home_page(request):
