b63bf87b600dca980ba79b085db3f8cd041fbf40
Yasemin Tangül <ytangul1@ford.com.tr>
2025-05-12 02:34:54 +0300

    fix landing page

--- backend/landing/views.py
+++ backend/landing/views.py
@@ -6,19 +6,32 @@ from db.models import User
 from django.contrib.auth import authenticate, login
 
 def landing_page(request):
-    ...
-    if request.method == 'POST' and "login" in request.POST:
-        login_form = LoginForm(request.POST)
-        if login_form.is_valid():
-            username = login_form.cleaned_data["username"]
-            password = login_form.cleaned_data["password"]
+    login_form = LoginForm()
+    reg_form = RegistrationForm()
 
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)  # ✅ This sets request.user!
-                return redirect('profile')
-            else:
-                login_form.add_error(None, "Invalid credentials")
+    if request.method == "POST":
+        if "login" in request.POST:
+            login_form = LoginForm(request.POST)
+            if login_form.is_valid():
+                username = login_form.cleaned_data["username"]
+                password = login_form.cleaned_data["password"]
+
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)  
+                    return redirect("profile")
+                else:
+                    login_form.add_error(None, "Invalid credentials")
+        else:
+            reg_form = RegistrationForm(request.POST)
+            if reg_form.is_valid():
+                reg_form.save()
+                return redirect("landing_page")
+
+    return render(request, "landing/index.html", {
+        "login_form": login_form,
+        "form": reg_form
+    })
 
 
 from django.contrib.auth.decorators import login_required
