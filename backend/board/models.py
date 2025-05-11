094342ffab01834ef63e3c8e34002fa5f2e59c15
Yasemin Tangül <ytangul1@ford.com.tr>
2025-05-12 00:42:44 +0300

    remove model defined user from board

--- backend/board/models.py
+++ backend/board/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class Board(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='boards')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.title
