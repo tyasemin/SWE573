a18fb0a5855ac0ba7540ab282c9e2127e4d5cc3e
Yasemin Tangül <ytangul1@ford.com.tr>
2025-05-12 00:47:10 +0300

    remove second defined board model

--- backend/board/models.py
+++ backend/board/models.py
@@ -1,13 +1,1 @@
-from django.db import models
-from django.contrib.auth.models import User
-from django.conf import settings
-
-class Board(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-
-    def __str__(self):
-        return self.title
 
