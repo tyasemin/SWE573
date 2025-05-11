af49d530f183c6b8dc291301db87cf0accc7ddae
Yasemin Tangül <ytangul1@ford.com.tr>
2025-05-11 23:32:41 +0300

    change user setting in django

--- backend/swe573_app/settings.py
+++ backend/swe573_app/settings.py
@@ -20,7 +20,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+AUTH_USER_MODEL = 'db.User'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
